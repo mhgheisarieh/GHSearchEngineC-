add6ad89db75d0e10289d250302a8528768de8fd
M.H Gheisarieh <mgheysariyeh@gmail.com>
2020-02-19 16:46:48 +0330

    Add project files.

---
+++ ConsoleApp1/Printable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GHSearchEngine
+{
+    interface IPrintable
+    {
+          void printResults(ArrayList documents, ArrayList results);
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GHSearchEngine
+{
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
