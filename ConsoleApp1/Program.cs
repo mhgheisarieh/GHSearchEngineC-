a86b79a5296f12290352c69285a87ba52136ba29
M.H Gheisarieh <mgheysariyeh@gmail.com>
2020-03-28 07:49:48 +0430

    read data from database and print

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,7 +15,9 @@ namespace GHSearchEngine
 
         static void Main(string[] args)
         {
-                        DocumentHolder documentHolder = new DocumentHolder(new CSVFileReader().readCSVFile(FILE_NAME));
+            SqlServerConnector sqlServerConnector = new SqlServerConnector();
+            sqlServerConnector.connectToServer();
+                        //DocumentHolder documentHolder = new DocumentHolder(new CSVFileReader().readCSVFile(FILE_NAME));
             /*            PreProcessedData.getInstance().setDetailsOfWordHashMap(PreProcessor.preProcess(documentHolder.getDocuments()));
                         IPrintable printer = new Printer();
                         SearchEngine searchEngine = new SearchEngine(documentHolder, printer);
--- ConsoleApp1/sqlServerConnector.cs
+++ ConsoleApp1/sqlServerConnector.cs
@@ -1,36 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Text;
 
 namespace GHSearchEngine
 {
-    class sqlServerConnector
+    class SqlServerConnector
     {
-        private static void connectToServer()
+        public void connectToServer()
         {
-            try
+            DataTable textTable = extraceDataFromDatabase();
+            foreach (DataRow row in textTable.Rows)
             {
-                // Build connection string
-                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-                builder.DataSource = "localhost";   // update me
-                builder.UserID = "sa";              // update me
-                builder.Password = "root";      // update me
-                builder.InitialCatalog = "master";
-
-                // Connect to SQL
-                Console.Write("Connecting to SQL Server ... ");
-                using SqlConnection connection = new SqlConnection(builder.ConnectionString);
-                connection.Open();
-                Console.WriteLine("Done.");
+                Console.WriteLine(row["\"Text\""]);
             }
-            catch (SqlException e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-
             Console.WriteLine("All done. Press any key to finish...");
             Console.ReadKey(true);
+        }
+
+        private static DataTable extraceDataFromDatabase()
+        {
+            DataTable textTable;
+            SqlConnectionStringBuilder builder = buildSqlConnectioStringBuilder();
+            SqlConnection connection = new SqlConnection(builder.ConnectionString);
+            connection.Open();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter("Select * from English", connection);
+            textTable = new DataTable();
+            dataAdapter.Fill(textTable);
+            return textTable;
+        }
+
+        private static SqlConnectionStringBuilder buildSqlConnectioStringBuilder()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "localhost";
+            builder.UserID = "sa";
+            builder.Password = "root";
+            builder.InitialCatalog = "English_csv_3";
+            builder.IntegratedSecurity = true;
+            return builder;
         }
     }
 }
